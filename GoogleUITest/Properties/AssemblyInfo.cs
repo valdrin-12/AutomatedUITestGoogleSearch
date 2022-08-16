427f7b339211c9db4bd1faaedc3ffb087497cd1b
valdrin-12 <109156924+valdrin-12@users.noreply.github.com>
2022-08-16 17:32:58 +0200

    AutomatedUITestGoogleSearchOK
    TestSearchStreetfighterthenverifystreetIsDisplayed

---
+++ GoogleUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bfbd74ce-2cde-4c3e-9794-4d88c536ab37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GoogleUITest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace GoogleUITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSearchStreetfighterthenverifystreetIsDisplayed()
+        {
+            By googleSearchBar = By.Name("q");
+            //IWebElement googleSearchbutton =WebDriver.FindElement(By.CssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.UUbT9 > div.aajZCb > div.CqAVzb.lJ9FBc > center > input.gNO89b"));
+           By googleResultText = By.XPath(".//h2//span[text()='Street Fighter V']");
+
+
+            IWebDriver webDriver = new ChromeDriver();
+            //webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl("https://www.google.co.uk/");
+
+            IWebElement googleSearchbutton = webDriver.FindElement(By.CssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b"));
+
+           // IWebElement googleResultText = webDriver.FindElement(By.XPath(".//h2//span[text()='Street Fighter V']"));
+
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
+
+            googleSearchbutton.Click();
+            // webDriver.FindElement(googleSearchbutton).Submit();
+
+            var actualResultText= webDriver.FindElement(googleResultText);
+            Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+            webDriver.Quit();   
+
+
+
+
+
+
+        }
+    }
+}
